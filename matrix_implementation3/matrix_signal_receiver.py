12484f530a54026209858771608d05a3fda6a38d
eduardohufg <eduardochavezmartin10@gmail.com>
2025-02-12 00:53:40 -0600

    add: matrix

---
+++ matrix_implementation3/matrix_signal_receiver.py
@@ -0,0 +1,57 @@
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import Int8
+import smbus
+import time
+
+I2C_SLAVE_ADDR = 0x08 
+
+class MatrixSignalReceiver(Node):
+    def __init__(self):
+
+        super().__init__('matrix_signal_receiver')
+        self.subscription = self.create_subscription(
+            Int8,
+            '/matrix_signal',
+            self.listener_callback,
+            10)
+        self.subscription
+
+        self.bus = smbus.SMBus(1)
+
+        self.matrix_signal_to_color_dict = {
+            0: "matrix_off",
+            1: "blue",
+            2: "red",
+            3: "green",
+            4: "quantum"
+        }
+        self.matrix_color = self.matrix_signal_to_color_dict[1]
+
+    def send_data(self, data):
+        self.bus.write_byte(I2C_SLAVE_ADDR, int(data))
+        print("Data sent")
+
+    def listener_callback(self, msg):
+
+        self.matrix_color = self.matrix_signal_to_color_dict[msg.data]
+
+        if self.matrix_color == "matrix_off":
+            self.send_data("0")
+        elif self.matrix_color == "blue":
+            self.send_data("1")
+        elif self.matrix_color == "red":
+            self.send_data("2")
+        elif self.matrix_color == "green":
+            self.send_data("3")
+
+def main(args=None):
+    rclpy.init(args=args)
+    matrix_signal_receiver = MatrixSignalReceiver()
+    rclpy.spin(matrix_signal_receiver)
+    matrix_signal_receiver.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
+
---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+package_name = 'matrix_implementation3'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='jetson',
+    maintainer_email='eduardochavezmartin10@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [ 'matrixsignalreceiver = matrix_implementation3.matrix_signal_receiver:main'
+        ],
+    },
+)
